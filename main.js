d4f1fe926fee2e2882f4d353dc6f2d2ef7e65a04
Lawrence <146691376+cedik456@users.noreply.github.com>
2024-08-04 22:31:06 +0800

    Finished Toast Notification/Snack Bar Using HTML CSS and JavaScript
    few more js projects to go

---
+++ main.js
@@ -0,0 +1,25 @@
+
+const toastBox = document.getElementById("toastBox");
+
+const successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted!'; 
+const errorMsg = `<i class="fa-solid fa-circle-xmark"></i> Please Fix the error !`;
+const invalidMsg = `<i class="fa-solid fa-circle-exclamation"></i> Invalid input, Please check again`;
+
+function showToast(msg){
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = msg;
+  toastBox.appendChild(toast);
+
+  if(msg.includes('error')){
+    toast.classList.add("error");
+  }
+
+  if(msg.includes('Invalid')){
+    toast.classList.add("invalid");
+  }
+
+  setTimeout(() => {
+    toast.remove()
+  }, 6000)
+}
